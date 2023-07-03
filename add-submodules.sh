d66c3d616ca7223e0c30f2b2ad723aca44a8c37b
Nils Ratnaweera <rata@zhaw.ch>
2023-07-03 11:53:41 +0200

    added commit date

--- add-submodules.sh
+++ add-submodules.sh
@@ -1,1 +1,6 @@
+
+# add all submodules
 awk -F, '{ system("git submodule add " $2 " " $1)}' df.csv
+
+# get last commit of each submodule and add export as csv
+git submodule foreach --quiet 'echo "$path",$(git log -1 --format="%ai")' > submodule_commits.csv
